7f926162d8b8bc7e77a89f96b70a74b72656d1c8
anshu <anshuman.moharana07@gmail.com>
2024-01-20 20:49:48 +0530

    new commit

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const age=23;
 const name="anshuman moharana";
 //in order to add file to git the command is git add fileName
 //in order to add all the files the command is git add .
+const classe = "btech";
