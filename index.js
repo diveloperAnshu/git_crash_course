bf55591c71eb0bbf968c73893a5ef11e162f1f8d
Anshuman_Moharana <anshuman.moharana07@gmail.com>
2024-01-20 23:07:52 +0530

    commited with new user name

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const name="anshuman moharana";
 //in order to add file to git the command is git add fileName
 //in order to add all the files the command is git add .
 const classe = "btech";
+//a;dhgweui9r
+/*-dewdededawd
+sadadasd*/
